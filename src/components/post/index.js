cee0424c0fc07f2c0bad525e088c1355290b223a
Robert Shields <robert.shieldsiv@gmail.com>
2021-08-07 15:16:20 -0700

    creating post component

---
+++ src/components/post/index.js
@@ -0,0 +1,25 @@
+import React, { useRef } from 'react';
+
+import Header from './header';
+import Image from './image';
+import Actions from './actions';
+import Footer from './footer';
+import Comments from './comments';
+
+export default function Post({ content }) {
+    const commentInput = useRef(null);
+    const handleFocus = () => commentInput.current.focus();
+    
+    return (
+        <div className="rounded col-span-4 border bg-white mb-16">
+            <Image src={content.imageSrc} caption={content.caption} />
+            <Actions
+                docId={content.docId}
+                totalLikes={content.likes.length}
+                likedPhoto={content.userLikedPhoto}
+                handleFocus={handleFocus}
+            />
+            <Footer username={content.username} caption={content.caption} />
+        </div>
+    )
+}
--- src/seed.js
+++ src/seed.js
@@ -16,7 +16,7 @@ export function seedDatabase(firebase) {
         fullName: 'Raffaello Sanzio da Urbino',
         emailAddress: 'raphael@sanzio.com',
         following: [],
-        followers: ['RwSP88IAlNXEr8ORTHFF3bmFwjN2'],
+        followers: ['bmjpkU8bBWRse2JwKgzOGTv5tUI3'],
         dateCreated: Date.now()
       },
       {
@@ -25,7 +25,7 @@ export function seedDatabase(firebase) {
         fullName: 'Salvador Dalí',
         emailAddress: 'salvador@dali.com',
         following: [],
-        followers: ['RwSP88IAlNXEr8ORTHFF3bmFwjN2'],
+        followers: ['bmjpkU8bBWRse2JwKgzOGTv5tUI3'],
         dateCreated: Date.now()
       },
       {
@@ -34,7 +34,7 @@ export function seedDatabase(firebase) {
         fullName: 'George Orwell',
         emailAddress: 'george@orwell.com',
         following: [],
-        followers: ['RwSP88IAlNXEr8ORTHFF3bmFwjN2'],
+        followers: ['bmjpkU8bBWRse2JwKgzOGTv5tUI3'],
         dateCreated: Date.now()
       }
     ];
@@ -69,4 +69,3 @@ export function seedDatabase(firebase) {
         });
     }
   }
-  
