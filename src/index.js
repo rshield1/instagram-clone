2fe28a4bf769939b7f8721320d5ecf1d018ede54
Robert Shields <robert.shieldsiv@gmail.com>
2021-08-06 23:12:32 -0700

    added login page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { firebase, FieldValue } from './lib/firebase'
+import App from './App';
 import FirebaseContext from './context/firebase';
 
 ReactDOM.render(
   <FirebaseContext.Provider value={{ firebase, FieldValue }}>
-    <h1 className="text-red-500">Hello, World</h1>
+    <App />
   </FirebaseContext.Provider>,
   document.getElementById('root')
 );
